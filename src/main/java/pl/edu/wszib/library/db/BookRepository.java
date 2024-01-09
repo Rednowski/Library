76f894eb6871235aefcf72571a8cf3826fc23fbb
Rednowski <kuba.rednowski@gmail.com>
2024-01-09 10:06:06 +0100

    changed array to arraylist

--- src/main/java/pl/edu/wszib/library/db/BookRepository.java
+++ src/main/java/pl/edu/wszib/library/db/BookRepository.java
@@ -3,21 +3,22 @@ package pl.edu.wszib.library.db;
 import pl.edu.wszib.library.book.Book;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BookRepository {
     Scanner scanner = new Scanner(System.in);
-    private final Book[] books = new Book[5];
+    private final ArrayList<Book> books = new ArrayList<>();
 
     public BookRepository(){
-        this.books[0] = new Book("Dzieci z Bullerbyn","Astrid Lindgren", 1234567890120L);
-        this.books[1] = new Book("Chłopi","Władysław Reymont", 1234567890121L);
-        this.books[2] = new Book("Pan Tadeusz","Adam Mickiewicz", 1234567890122L);
-        this.books[3] = new Book("Dziady","Adam Mickiewicz", 1234567890123L);
-        this.books[4] = new Book("Harry Potter","J.K. Rowling", 1234567890124L);
+        this.books.add(new Book("Dzieci z Bullerbyn","Astrid Lindgren", 1234567890120L));
+        this.books.add(new Book("Chłopi","Władysław Reymont", 1234567890121L));
+        this.books.add(new Book("Pan Tadeusz","Adam Mickiewicz", 1234567890122L));
+        this.books.add(new Book("Dziady","Adam Mickiewicz", 1234567890123L));
+        this.books.add(new Book("Harry Potter","J.K. Rowling", 1234567890124L));
     }
 
-    public Book[] getBooks(){
+    public ArrayList<Book> getBooks(){
         return books;
     }
 
