59d238e8314359f8b753c637de3712afcc9ac377
Rednowski <kuba.rednowski@gmail.com>
2024-02-16 19:07:15 +0100

    rent now updates db

--- src/main/java/pl/edu/wszib/library/db/BookRepository.java
+++ src/main/java/pl/edu/wszib/library/db/BookRepository.java
@@ -3,6 +3,7 @@ package pl.edu.wszib.library.db;
 import pl.edu.wszib.library.App;
 import pl.edu.wszib.library.book.Book;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -44,16 +45,43 @@ public class BookRepository {
     }
 
     public boolean rent(long isbn){
-        for(Book book : this.books){
-            if(book.getIsbn() == isbn && !book.isRent()){
-                System.out.println("Enter your fullname");
-                String fullName = scanner.nextLine();
-                book.setfullName(fullName);
-                book.setRent(true);
-                book.setStartDate(LocalDate.now());
-                book.setEndDate(LocalDate.now().plusDays(14));
-                return true;
+//        for(Book book : this.books){
+//            if(book.getIsbn() == isbn && !book.isRent()){
+//                System.out.println("Enter your fullname");
+//                String fullName = scanner.nextLine();
+//                book.setfullName(fullName);
+//                book.setRent(true);
+//                book.setStartDate(LocalDate.now());
+//                book.setEndDate(LocalDate.now().plusDays(14));
+//                return true;
+//            }
+//        }
+        try{
+            String sql = "SELECT * FROM tbook WHERE isbn = ?";
+            PreparedStatement preparedStatement = App.connection.prepareStatement(sql);
+            preparedStatement.setLong(1, isbn);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            if(rs.next()){
+                boolean rent = rs.getBoolean("rent");
+                if(!rent){
+                    System.out.println("Enter your fullname");
+                    String fullName = scanner.nextLine();
+                    int id = rs.getInt("id");
+                    String updateSql = "UPDATE tbook SET rent = ?, fullname = ?, startDate = ?, endDate = ? WHERE id = ?";
+                    PreparedStatement updatePreparedStatement = App.connection.prepareStatement(updateSql);
+                    updatePreparedStatement.setBoolean(1, true);
+                    updatePreparedStatement.setString(2, fullName);
+                    updatePreparedStatement.setDate(3, Date.valueOf(LocalDate.now()));
+                    updatePreparedStatement.setDate(4, Date.valueOf(LocalDate.now().plusDays(14)));
+                    updatePreparedStatement.setInt(5, id);
+
+                    updatePreparedStatement.executeUpdate();
+                    return true;
+                }
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return false;
     }
